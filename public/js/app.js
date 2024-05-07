e017743ae6e6f5b5449fa958ba25435e9a53f529
RuthMaciel <ruthmaciel12334@gmail.com>
2024-05-07 14:53:38 -0400

    parte de Ruth

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var docentes =[]
+var docentes = []
 const doceId = document.getElementById('doceId');
 const doceName = document.getElementById('doceName');
 const doceLastName = document.getElementById('doceLastName');
@@ -49,8 +49,8 @@ function showDocente() {
 }
 
 //funcion para pasar los datos al formulario de editar 
-function editFormsDocente(e){  //recibimos de parametro un evento 
-    addFormDoce.classList.toggle( 'hidden');
+function editFormsDocente(e) {  //recibimos de parametro un evento 
+    addFormDoce.classList.toggle('hidden');
     editFormsDocente.classList.toggle('hidden');
     backBtnFormDoce.classlist.toggle('hidden');
     const doceIdInput = e.target.getAttribute('data-id'); // vemos el target del evento y obtenemos el data id
@@ -58,16 +58,16 @@ function editFormsDocente(e){  //recibimos de parametro un evento
         if (d.doceId == doceIdInput) { // verificamos si el doceID es igual al doceInput que agarramos del data id
             document.getElementById('doceIdEdit').value = d.doceId;// guardamos en los input los valores de cada rato del arreglo
             document.getElementById('doceNameEdit').value = d.doceName;
-            document.getElementById('doceLastNameEdit').value =d.doceLastName;
+            document.getElementById('doceLastNameEdit').value = d.doceLastName;
             document.getElementById('doceEmailEdit').value = d.doceEmail;
             document.getElementById('doceBirthEdit').value = d.doceBirth
             document.getElementById('doceCelEdit').value = d.doceCel;
         }
-     })
-}     
+    })
+}
 
 
-function editDocente () {
+function editDocente() {
     const doceIdInput = document.getElementById('doceIdEdit').value; // obtenemos el value de doceId para saber que editamos 
     docentes.forEach((d)) => { //volvemos a recorrer el arreglo 
         if (d.doceId == doceIdInput) {//verificamos si doceId es igual al valor que recuperamos de doceId
@@ -78,30 +78,30 @@ function editDocente () {
             d.doceCel = document.getElementById('doceCelEdit').value;
 
         }
-  })
-  localStorage.setItem('docentes', JSON.stringify(docentes)); // guardamos en el local storage el arreglo modificado
-  showDocente();//mostramos la tabla de docentes
+    })
+    localStorage.setItem('docentes', JSON.stringify(docentes)); // guardamos en el local storage el arreglo modificado
+    showDocente();//mostramos la tabla de docentes
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-    }  )
-
-
-
-
-
-}
-
 addBtn.addEventListener('click', addDocente);
+//funcion para eliminar
+function deleteElementDocente(e) { //recibimos el evento como parametro
+    const doceIdInput = e.target.getAttribute('data-id'); //conseguimos el data id
+    docentes.forEach((d, i) => { //recorremos el arreglo
+        if (d.doceId == doceIdInput) { // verificamos si el doceID es igual al data id recuperado
+            docentes.splice(i, 1); //eliminamos esos datos de docente del arreglo
+        }
+    })
+    localStorage.setItem('docentes', JSON.stringify(docentes)); //guardamos los cambios del arreglo en el local storage
+    showDocente();
+}
+//una funcion con un bucle que detecta cuando se hace click en boton editar o eliminar del docente y llama la funcion correspondiente
+function eventosDocente() {
+    var btseditar = document.getElementsByClassName("bteditDocente");
+    for (let i = 0; i < btseditar.length; i++) {
+        btseditar[i].addEventListener("click", editFormDocente);
+    }
+    var btsborrar = document.getElementsByClassName("btborrarDocente");
+    for (let i = 0; i < btsborrar.length; i++) {
+        btsborrar[i].addEventListener("click", deleteElementDocente)
+    }
+}
